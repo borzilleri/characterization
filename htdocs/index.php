acc52510d319e0fca66169a2adb7f2a5012088f4
Jonathan Borzilleri <jonathan.borzilleri@gmail.com>
2009-08-06 00:32:34 -0700

    - More styling of the power list. - Add a power box display to the power edit form - Sort the character list by character name - Style the character list -

--- htdocs/index.php
+++ htdocs/index.php
@@ -1,15 +1,26 @@
 <?php
 include('autohandler.php');
 
-$char_list = Doctrine_Query::create()->from('Player p')->execute();
-?>
+$char_list = Doctrine_Query::create()
+  ->from('Player p')->orderBy('p.name')->execute();
 
-<ul>
-  <? foreach($char_list as $c): ?>
-  <li><a href="<?=SITE_URL;?>/<?=$c->id;?>";?><?=$c->name;?></a> 
-    (<?=$c->Race->name;?> <?=$c->Archetype->name;?> <?=$c->level;?>)
+$i = 0;
+?>
+<ul class="list">
+<? foreach($char_list as $c): ?>
+  <li class="row<?=$i;$i=($i+1)%2;?>">
+    <div class="listLinks">
+      [<a href="<?=SITE_URL;?>/<?=$c->id;?>">view</a>&nbsp;|
+      <a href="<?=SITE_URL;?>/<?=$c->id;?>/edit">edit</a>]
+    </div>
+    <div class="listLabel">
+      <?=$c->name;?>
+    </div>
+    <div class="listInfo">
+      <?=$c->Race->name;?> <?=$c->Archetype->name;?> <?=$c->level;?>
+    </div>
   </li>
-  <? endforeach; ?>
+<? endforeach; ?>
 </ul>
 
 <?php include('footer.php'); ?>
