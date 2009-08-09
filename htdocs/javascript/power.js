17a27fdccbe441f153776737afcbb6be4a3b0aea
Jonathan Borzilleri <jonathan.borzilleri@gmail.com>
2009-08-09 16:38:09 -0700

    - Sort the initially loaded power list by usage, level, name, in that order - Add toggleable asc/desc sorting to the power list - Fix the alternating background colors in the power list when the list is sorted.

--- htdocs/javascript/power.js
+++ htdocs/javascript/power.js
@@ -28,70 +28,183 @@ function previewPower(id) {
 	);
 }
 
-function sortByUsage(a, b) {
-	var a_usage = $(a).find('span.use_type').text();
+function getSortableUsage(usage) {
+	switch(usage) {
+		case 'At-Will':
+			return '1_'+usage;
+			break;
+		case 'Encounter':
+			return '2_'+usage;
+			break;
+		case 'Daily':
+			return '3_'+usage;
+			break;
+		default:
+			return usage;
+			break;
+	}
+}
+
+function sortByUsage(a, b, asc) {
+	var a_usage = getSortableUsage($(a).find('span.use_type').text());
 	var a_level = $(a).find('span.level').text();
 	var a_name = $(a).find('span.power_name').text();
 	
-	var b_usage = $(b).find('span.use_type').text();
+	var b_usage = getSortableUsage($(b).find('span.use_type').text());
 	var b_level = $(b).find('span.level').text();
 	var b_name = $(b).find('span.power_name').text();
 	
 	if( a_usage == b_usage ) {
 		if( a_level == b_level ) {
-			return a_name > b_name ? 1 : -1;
+			if( asc ) return a_name > b_name ? 1 : -1;
+			else return a_name < b_name ? 1 : -1;
 		}
 		else {
-			return a_level > b_level ? 1 : -1;
+			if( asc ) return a_level > b_level ? 1 : -1;
+			else return a_level < b_level ? 1 : -1;
 		}
 	}
 	else {
-		return a_usage > b_usage ? 1 : -1;
+		if( asc ) return a_usage > b_usage ? 1 : -1;
+		else return a_usage < b_usage ? 1 : -1;
 	}
-	return $(a).find('span.use_type').text() > $(b).find('span.use_type').text() ? 1 : -1;
 }
-function sortByLevel(a, b) {
-	var a_usage = $(a).find('span.use_type').text();
+function sortByUsageAsc(a, b) {
+	return sortByUsage(a, b, true);
+}
+function sortByUsageDesc(a, b) {
+	return sortByUsage(a, b, false);
+}
+
+
+function sortByLevel(a, b, asc) {
+	var a_usage = getSortableUsage($(a).find('span.use_type').text());
 	var a_level = $(a).find('span.level').text();
 	var a_name = $(a).find('span.power_name').text();
 	
-	var b_usage = $(b).find('span.use_type').text();
+	var b_usage = getSortableUsage($(b).find('span.use_type').text());
 	var b_level = $(b).find('span.level').text();
 	var b_name = $(b).find('span.power_name').text();
 	
 	if( a_level == b_level ) {
 		if( a_usage == b_usage ) {
-			return a_name > b_name ? 1 : -1;
+			if( asc ) return a_name > b_name ? 1 : -1;
+			else return a_name < b_name ? 1 : -1;
 		}
 		else {
-			return a_usage > b_usage ? 1 : -1;
+			if( asc ) return a_usage > b_usage ? 1 : -1;
+			else return a_usage < b_usage ? 1 : -1;
 		}
 	}
 	else {
-		return a_level > b_level ? 1 : -1;
+		if( asc ) return a_level > b_level ? 1 : -1;
+		else return a_level < b_level ? 1 : -1;		
 	}	
-	
-	return $(a).find('span.level').text() > $(b).find('span.level').text() ? 1 : -1;
 }
-function sortByName(a, b) {
-	return $(a).find('span.power_name').text() > $(b).find('span.power_name').text() ? 1 : -1;
+function sortByLevelAsc(a, b) {
+	return sortByLevel(a, b, true);
+}
+function sortByLevelDesc(a, b) {
+	return sortByLevel(a, b, false);
 }
 
-function sortPowerList(sortBy) {
+function sortByName(a, b, asc) {
+	var a_name = $(a).find('span.power_name').text();
+	var b_name = $(b).find('span.power_name').text();
+	if( asc ) return a_name > b_name ? 1 : -1;
+	else return a_name < b_name ? 1 : -1
+}
+function sortByNameAsc(a, b) {
+	return sortByName(a, b, true);
+}
+function sortByNameDesc(a, b) {
+	return sortByName(a, b, false);
+}
+
+function updateSortLinks(sortBy, asc) {
+	// Name sort link
+	if( 'level' == sortBy || 'use_type' == sortBy ) {
+		$('#power_name').removeClass('desc').addClass('asc');
+		$('#name_arr').html('');
+	}
+	else if( 'power_name' == sortBy ) {
+		if( asc ) {
+			$('#power_name').removeClass('asc').addClass('desc');
+			$('#name_arr').html('&uarr;');
+		}
+		else {
+			$('#power_name').removeClass('desc').addClass('asc');
+			$('#name_arr').html('&darr;');
+		}
+	}
+	
+	// Level Sort link
+	if( 'power_name' == sortBy || 'use_type' == sortBy ) {
+		$('#level').removeClass('desc').addClass('asc');
+		$('#level_arr').html('');
+	}
+	else if( 'level' == sortBy ) {
+		if( asc ) {
+			$('#level').removeClass('asc').addClass('desc');
+			$('#level_arr').html('&uarr;');
+		}
+		else {
+			$('#level').removeClass('desc').addClass('asc');
+			$('#level_arr').html('&darr;');
+		}
+	}
+	
+	// Usage Sort Link
+	if( 'power_name' == sortBy || 'level' == sortBy ) {
+		$('#use_type').removeClass('desc').addClass('asc');
+		$('#usage_arr').html('');
+	}
+	else if( 'use_type' == sortBy ) {
+		if( asc ) {
+			$('#use_type').removeClass('asc').addClass('desc');
+			$('#usage_arr').html('&uarr;');
+		}
+		else {
+			$('#use_type').removeClass('desc').addClass('asc');
+			$('#usage_arr').html('&darr;');
+		}
+	}
+}
+
+function sortPowerList(link) {
+	var sortBy = link.id;
+	var asc = $(link).hasClass('asc');
+	
 	switch(sortBy) {
 		case 'power_name':
-			$('li.powerItem').sort(sortByName).appendTo('ul.list');
+			if( asc ) $('li.powerItem').sort(sortByNameAsc).appendTo('ul.list');
+			else $('li.powerItem').sort(sortByNameDesc).appendTo('ul.list');
+			updateSortLinks(sortBy, asc)
 			break;
 		case 'level':
-			$('li.powerItem').sort(sortByLevel).appendTo('ul.list');
+			if( asc ) $('li.powerItem').sort(sortByLevelAsc).appendTo('ul.list');
+			else $('li.powerItem').sort(sortByLevelDesc).appendTo('ul.list');
+			updateSortLinks(sortBy, asc)
 			break;
 		case 'use_type':
-			$('li.powerItem').sort(sortByUsage).appendTo('ul.list');
+			if( asc ) $('li.powerItem').sort(sortByUsageAsc).appendTo('ul.list');
+			else $('li.powerItem').sort(sortByUsageDesc).appendTo('ul.list');
+			updateSortLinks(sortBy, asc)
 			break;
 	}
+	
+	var i = 1;
+	$('li.powerItem').removeClass('row0');
+	$('li.powerItem').removeClass('row1');
+
+	$('li.powerItem').each(function() {
+		var rowColor = "row"+i;
+		i = (i+1)%2;
+		$(this).addClass(rowColor);
+	});
 }
 
 $(document).ready(function() {
 	$('.power_preview').click(function() { previewPower(this.id) });	
-	$('.sort').click(function() { sortPowerList(this.id) });
+	$('.sort').click(function() { sortPowerList(this) });
 });
